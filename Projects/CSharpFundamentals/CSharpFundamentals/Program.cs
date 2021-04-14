861cbf93b11cded151db6294642dc944a75a0ce1
sphillips431 <phillips.sean.m@gmail.com>
2021-04-13 22:22:08 -0500

    file exercise stuff

--- Projects/CSharpFundamentals/CSharpFundamentals/Program.cs
+++ Projects/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using CSharpFundamentals.Math;
 using System.Text;
+using System.IO;
 
 namespace CSharpFundamentals
 {
@@ -9,28 +10,42 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var sentence = "this is going to be a really really really really really long text.";
-            var summary = StringUtility.SummerizeText(sentence, 25);
-            Console.WriteLine(summary);
+            /* var sentence = "this is going to be a really really really really really long text.";
+             var summary = StringUtility.SummerizeText(sentence, 25);
+             Console.WriteLine(summary);
 
-            var builder = new StringBuilder();
+             var builder = new StringBuilder();
 
-            builder.Append('-', 10);
-            builder.AppendLine();
-            builder.Append("Header");
-            builder.AppendLine();
-            builder.Append('-', 10);
+             builder.Append('-', 10);
+             builder.AppendLine();
+             builder.Append("Header");
+             builder.AppendLine();
+             builder.Append('-', 10);
 
-            builder.Replace('-', '+');
+             builder.Replace('-', '+');
 
-            builder.Remove(0, 10);
-            Console.WriteLine(builder);
+             builder.Remove(0, 10);
+             Console.WriteLine(builder);
+            */
 
             // Console.WriteLine(StringExercises.ConsecutiveCheck());
 
             // Console.WriteLine(StringExercises.DuplicateCheck());
-            StringExercises.TimeCheck();
 
+            //StringExercises.TimeCheck();
+
+            var path = @"C:\Users\Sean and Kayla\Desktop\Sean_Work\Udemy\CSharp\file.txt";
+
+            var allWords = File.ReadAllText(path);
+            var words = allWords.Split(' ');
+
+            var count = 0;
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+                count++;
+            }
+            Console.WriteLine(count);
         }
 
         
